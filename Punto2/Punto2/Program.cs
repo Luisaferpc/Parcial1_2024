2bec4bfd9e35db17e06065ceb489d4929f56a32b
Luisaferpc <luisacardona2206@gmail.com>
2024-09-13 21:21:39 -0500

    cualquier numero ingresado va a tener solucion 0

--- Punto2/Punto2/Program.cs
+++ Punto2/Punto2/Program.cs
@@ -2,10 +2,15 @@
 {
     private static void Main(string[] args)
     {
+        
         Console.WriteLine("Ingrese el valor para coeficiente(a): ");
         double a=Convert.ToDouble(Console.ReadLine());
 
-        Console.WriteLine("\"Ingrese el valor para coeficiente(b): ");
+        Console.WriteLine("Ingrese el valor para coeficiente(b): ");
         double b=Convert.ToDouble(Console.ReadLine());
+       
+        double X = (-b / a);
+
+        Console.WriteLine("La solucion de la ecuacion es: " + ((a * X) + b));
     }
 }
