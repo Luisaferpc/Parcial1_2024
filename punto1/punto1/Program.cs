f2fe2a857f88e926d90bcfb01fb6ab9a920934a1
Luisaferpc <luisacardona2206@gmail.com>
2024-09-13 20:55:11 -0500

    estructura del 1 if

--- punto1/punto1/Program.cs
+++ punto1/punto1/Program.cs
@@ -5,7 +5,8 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        double sueldo=1500000, comision=0.10, venta1, venta2,venta3, beneficio=100000;
+        double sueldo = 1500000, Pcomision = 0.10, venta1, venta2, venta3, beneficio = 100000;
+        
 
         Console.WriteLine("Ingrese el valor de su primer venta: ");
         venta1=Convert.ToDouble(Console.ReadLine());
@@ -16,13 +17,18 @@ public class Program
         Console.WriteLine("Ingrese el valor de su tercer venta: ");
         venta3 = Convert.ToDouble(Console.ReadLine());
 
-        double ventatotal=venta1+venta2+venta3;
+        double ventatotal = venta1 + venta2 + venta3;
+        double Vcomision = ventatotal * Pcomision; //para calcular el valor total de la comision
 
-        Console.WriteLine("Total de comisiones del mes: " +  ventatotal*comision);
 
-        if(ventatotal > 1000000) 
+        if (ventatotal > 1000000) 
         {
             Console.WriteLine("Felicitaciones! Ha cumplido la meta del mes para ganar el beneficio.");
+            Console.WriteLine("Total de comisiones del mes: "  +Vcomision);
+            
+
+
+            Console.WriteLine("total a pagar(sueldo+comisiones+bonificacion ")
 
 
         }
