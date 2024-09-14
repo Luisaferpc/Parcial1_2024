8323209d19a51cacf464e2d3e986110ff673bbbb
Luisaferpc <luisacardona2206@gmail.com>
2024-09-13 21:15:33 -0500

    definiendo variables mientras desarrollo

--- Punto2/Punto2/Program.cs
+++ Punto2/Punto2/Program.cs
@@ -2,6 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        
+        Console.WriteLine("Ingrese el valor para coeficiente(a): ");
+        double a=Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\"Ingrese el valor para coeficiente(b): ");
+        double b=Convert.ToDouble(Console.ReadLine());
     }
 }
